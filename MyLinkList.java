ae4fedf705d2629498a319c39766291fc9a4cd6c
naveeny061 <naveeny061@gmail.com>
2020-11-30 19:31:00 +0530

    Added:Add method to link the nodes

--- MyLinkList.java
+++ MyLinkList.java
@@ -1,13 +1,52 @@
 package com.assignment;
 
-import com.linkList.MyNode;
-
 public class MyLinkList<K> {
+	public INode head;
+	public INode tail;
+
+	public MyLinkList() {
+
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void add(INode newNode) {
+		if (head == null)
+			this.head = newNode;
+		if (tail == null)
+			this.tail = newNode;
+		else {
+			INode tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	public void printMyNodes() {
+		StringBuffer myNode = new StringBuffer("My Nodes");
+		INode tempNode = head;
+		while (tempNode.getNext() != null) {
+			myNode.append(tempNode.getKey());
+			if (!tempNode.equals(tail))
+				myNode.append("->");
+			tempNode = tempNode.getNext();
+		}
+		myNode.append(tempNode.getKey());
+		System.out.println(myNode);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Linked List Problem");
-		MyNode<Integer> myFirstNode=new MyNode<Integer>(56);
-		MyNode<Integer> mySecondNode=new MyNode<Integer>(30);
-		MyNode<Integer> myThirdNode=new MyNode<	Integer>(70);	
+		MyLinkList myLinkList = new MyLinkList();
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+
+		myLinkList.add(myFirstNode);
+		myLinkList.add(mySecondNode);
+		myLinkList.add(myThirdNode);
+		myLinkList.printMyNodes();
+		
 	}
 
 }
