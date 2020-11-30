6cd731d8db91cc91e72a728d766f4d8f8fb1eeae
naveeny061 <naveeny061@gmail.com>
2020-11-30 19:21:43 +0530

    Added: Classes to implement simple link list

--- MyLinkList.java
+++ MyLinkList.java
@@ -1,8 +1,13 @@
 package com.assignment;
 
+import com.linkList.MyNode;
+
 public class MyLinkList<K> {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Linked List Problem");
+		MyNode<Integer> myFirstNode=new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode=new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode=new MyNode<	Integer>(70);	
 	}
 
 }
